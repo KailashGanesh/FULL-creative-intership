3c671ca38060c79773f63ee85a162cb4d3c3e44e
KailashGanesh <58307685+KailashGanesh@users.noreply.github.com>
2022-09-28 11:24:35 +0530

    when scroll header becomes sticky & bg change

--- Task-5-ws-2/src/script.js
+++ Task-5-ws-2/src/script.js
@@ -6,7 +6,39 @@ function closeNav(){
     document.getElementById("mobile-menu").style.width = "0";
 }
 
+function changeNavColor(color){
+    let white = "rgb(0,0,0,0.6)"
+    if (color == "white"){
+        header.style.backgroundColor = white;
+        // header.style.fill = "black";
+        // header.style.color = "black";
+        header.style.position = "fixed";
+        header.style.paddingTop = "28px"
+        header.style.paddingBottom = "14px"
+        console.log("turned white")
+    }else{
+        header.style.backgroundColor= "transparent";
+        // header.style.fill = "initial";
+        // header.style.color = "initial";
+        header.style.position = "absolute" ;
+        header.style.paddingTop = "56px"
+        header.style.paddingBottom = "0px"
+        console.log("turned initial")
+    }
+}
+
 console.log("hello")
 let header = document.getElementById("header");
 
 console.log(header);
+
+
+
+document.addEventListener('scroll', (e) => {
+    lastKnownScrollPosition = window.scrollY;
+    if (lastKnownScrollPosition >= 100){
+        changeNavColor("white")
+    }else if(lastKnownScrollPosition == 0){
+        changeNavColor()
+    }
+})
